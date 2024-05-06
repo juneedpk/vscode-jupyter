c05f68c51a8a136d5d8ba05682b9210d21ffc727
juneedpk <littledew32@gmail.com>
2024-05-06 23:50:44 +0500

    update

---
+++ pythonkachilla/streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+# adding title of app
+st.title('Paddy Disease Prediction App')
+# adding image to app
+from PIL import Image
+img = Image.open("E:\vscode\vscode-jupyter-1\pythonkachilla\paddy.PNG")
+# adding text to app
+st.write('Through this application we can do paddy disease prediction')
+
+
